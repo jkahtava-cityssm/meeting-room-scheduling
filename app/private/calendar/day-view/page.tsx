05fd1249ed53e72db605c98770356b0e65d75805
Jordan Kahtava <j.kahtava@cityssm.on.ca>
2025-05-22 08:29:06 -0400

    identified that server actions should not be used for GET requests.

--- app/private/calendar/day-view/page.tsx
+++ app/private/calendar/day-view/page.tsx
@@ -1,4 +1,6 @@
 import { CalendarDayView } from "@/components/calendar/calendar-day-view";
+import { useCalendar } from "@/components/calendar/contexts/calendar-context";
+import { getEventsDaily } from "@/services/events";
 
 export default async function Page() {
   return <CalendarDayView />;
