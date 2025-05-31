6f5e20e94efff625cbeb6eb06f583bde2c437cad
jkahtava-cityssm <j.kahtava@cityssm.on.ca>
2025-05-31 11:17:51 -0400

    adjusted room select to useSWR, fixed month scroll area, so it uses shadcn instead of default browser which fixed padding issue.

--- components/calendar/calendar-all-header-room-select.tsx
+++ components/calendar/calendar-all-header-room-select.tsx
@@ -1,63 +1,74 @@
 import { useCalendar } from "@/contexts/CalendarProvider";
-import { IRoom } from "@/lib/interfaces";
+
 import { TColors } from "@/lib/types";
 import { IconColored } from "@/components/ui/icon-colored";
 
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
-import { getRoomsWithAll } from "@/services/rooms";
+
 import { Asterisk, BookKey } from "lucide-react";
 import { useEffect, useState } from "react";
 import { Skeleton } from "../ui/skeleton";
+import useSWR from "swr";
+import { IRoom } from "@/lib/schemas/schemas";
 
 export function RoomSelect({ onRoomChange }: { onRoomChange: (value: string) => void }) {
-  const { selectedRoomId, setSelectedRoomId } = useCalendar();
+	const { selectedRoomId } = useCalendar();
 
-  const [isLoading, setIsLoading] = useState(true);
-  const [rooms, setRooms] = useState<IRoom[]>([]);
+	const [isLoading, setIsLoading] = useState(true);
+	const [rooms, setRooms] = useState<IRoom[]>([]);
 
-  const fetchRooms = async () => {
-    setIsLoading(true);
+	const { data } = useSWR<IRoom[]>(`/api/rooms`);
 
-    const rooms = await getRoomsWithAll();
+	useEffect(() => {
+		if (data) {
+			const allRooms: IRoom = {
+				roomId: -1,
+				color: "slate",
+				icon: "Asterisk",
+				name: "All Rooms",
+				createdAt: new Date(),
+				updatedAt: new Date(),
+			};
 
-    setRooms(rooms.data);
-    setIsLoading(false);
-  };
+			setRooms([allRooms, ...data]);
+			setIsLoading(false);
+		}
+	}, [data]);
 
-  useEffect(() => {
-    fetchRooms();
-  }, []);
+	if (isLoading) {
+		return <Skeleton className="flex w-fit items-center justify-between gap-2 shrink-0 h-9 flex-1 md:w-60"></Skeleton>;
+	}
 
-  if (isLoading) {
-    return <Skeleton className="flex w-fit items-center justify-between gap-2 shrink-0 h-9 flex-1 md:w-60"></Skeleton>;
-  }
+	return (
+		<Select
+			value={selectedRoomId.toString()}
+			onValueChange={value => {
+				onRoomChange(value);
+			}}
+		>
+			<SelectTrigger className="flex-1 md:w-60">
+				<SelectValue />
+			</SelectTrigger>
 
-  return (
-    <Select
-      value={selectedRoomId.toString()}
-      onValueChange={(value) => {
-        onRoomChange(value);
-      }}
-    >
-      <SelectTrigger className="flex-1 md:w-60">
-        <SelectValue />
-      </SelectTrigger>
-
-      <SelectContent align="end">
-        {rooms.map((room) => (
-          <SelectItem key={room.roomId} value={room.roomId.toString()} className="flex-1">
-            <div className="flex items-center gap-2">
-              <IconColored
-                hideBackground={false}
-                color={room.color as TColors}
-                showBorder={true}
-                children={room.roomId === -1 ? <Asterisk /> : <BookKey />}
-              ></IconColored>
-              <p className="truncate">{room.name}</p>
-            </div>
-          </SelectItem>
-        ))}
-      </SelectContent>
-    </Select>
-  );
+			<SelectContent align="end">
+				{rooms.map(room => (
+					<SelectItem
+						key={room.roomId}
+						value={room.roomId.toString()}
+						className="flex-1"
+					>
+						<div className="flex items-center gap-2">
+							<IconColored
+								hideBackground={false}
+								color={room.color as TColors}
+								showBorder={true}
+								children={room.roomId === -1 ? <Asterisk /> : <BookKey />}
+							></IconColored>
+							<p className="truncate">{room.name}</p>
+						</div>
+					</SelectItem>
+				))}
+			</SelectContent>
+		</Select>
+	);
 }
--- components/calendar/calendar-year-view-day-cell.tsx
+++ components/calendar/calendar-year-view-day-cell.tsx
@@ -14,48 +14,54 @@ import { DayView } from "./calendar-year-view";
 import { navigateURL } from "@/lib/helpers";
 
 const YearViewDayCell = ({ day }: { day: DayView }) => {
-  const { push } = useRouter();
-  const { setSelectedDate } = useCalendar();
+	const { push } = useRouter();
+	const { setSelectedDate } = useCalendar();
 
-  const maxIndicators = 3;
+	const maxIndicators = 3;
 
-  const handleClick = () => {
-    push(navigateURL(day.dayDate, "day"));
-  };
+	const handleClick = () => {
+		push(navigateURL(day.dayDate, "day"));
+	};
 
-  return (
-    <button
-      onClick={handleClick}
-      type="button"
-      className="flex h-11 flex-1 flex-col items-center justify-start gap-0.5 rounded-md pt-1 hover:bg-accent focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring"
-    >
-      <div
-        className={cn(
-          "flex size-6 items-center justify-center rounded-full text-xs font-medium",
-          day.isToday && "bg-primary font-semibold text-primary-foreground"
-        )}
-      >
-        {day.day}
-      </div>
-      {
-        <div className="mt-0.5 flex gap-0.5">
-          {day.dayEvents.length <= maxIndicators ? (
-            day.dayEvents.map((event, index) => (
-              <IconDot key={`day-${day.day}-${event.eventId}-${index}`} color={event.room.color as TColors}></IconDot>
-            ))
-          ) : (
-            <>
-              <IconDot
-                key={`day-${day.day}-${day.dayEvents[0].eventId}`}
-                color={day.dayEvents[0].room.color as TColors}
-              ></IconDot>
-              <span className="text-[7px] text-muted-foreground">+{day.dayEvents.length - 1}</span>
-            </>
-          )}
-        </div>
-      }
-    </button>
-  );
+	return (
+		<button
+			onClick={handleClick}
+			type="button"
+			className="flex h-11 flex-1 flex-col items-center justify-start gap-0.5 rounded-md pt-1 hover:bg-accent focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring"
+		>
+			<div
+				className={cn(
+					"flex size-6 items-center justify-center rounded-full text-xs font-medium",
+					day.isToday && "bg-primary font-semibold text-primary-foreground"
+				)}
+			>
+				{day.day}
+			</div>
+			{
+				<div className="mt-0.5 flex gap-0.5">
+					{day.dayEvents.length <= maxIndicators ? (
+						day.dayEvents.map((event, index) => (
+							<IconDot
+								key={`day-${day.day}-${event.eventId}-${index}`}
+								color={event.room.color as TColors}
+							></IconDot>
+						))
+					) : (
+						<>
+							<div className="flex justify-center items-center">
+								<IconDot
+									key={`day-${day.day}-${day.dayEvents[0].eventId}`}
+									color={day.dayEvents[0].room.color as TColors}
+								></IconDot>
+							</div>
+
+							<span className="text-[0.5rem] ">+ {day.dayEvents.length - 1}</span>
+						</>
+					)}
+				</div>
+			}
+		</button>
+	);
 };
 
 export default YearViewDayCell;
