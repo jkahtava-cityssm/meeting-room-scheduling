eee8bb6e312c5efd5043786e966ef510b9a7d862
Jordan Kahtava <j.kahtava@cityssm.on.ca>
2025-07-11 11:34:09 -0400

    moved logic into contextprovider to reduce re-renders and some bugs associated with zustand store.

--- contexts/EventFormProvider.tsx
+++ contexts/EventFormProvider.tsx
@@ -15,6 +15,8 @@ interface IEventFormContext {
   setReadOnly: (value: boolean) => void;
   currentForm: UseFormReturn<any, any, any> | undefined;
   setCurrentForm: (value: UseFormReturn<any, any, any>) => void;
+  defaultValues: object[];
+  setFormDefaultData: (data: object, step?: number) => void;
   currentStep: number;
   //setCurrentStep: (value: number) => void;
   formId: string;
@@ -22,7 +24,7 @@ interface IEventFormContext {
   setKeyData: (key: string, value: string, step: number) => void;
   getKeyData: (step: number, key: string) => string | undefined;
   getStepData: (step: number) => object;
-  setFormData: (data: object, id: number, step?: number) => void;
+  setFormData: (data: object, step?: number) => void;
   getFormData: (schema: z.ZodObject, defaultValues: object) => object;
   incrementStep: (step?: number) => void;
   decrementStep: (step?: number) => void;
@@ -38,11 +40,15 @@ export function EventFormProvider({ children }: { children: React.ReactNode }) {
   const { setSessionFormData, setSessionStep, getSessionState, setSessionKeyData, resetSessionFormData } =
     useFormStore();
 
-  const currentStep = useFormStore((state) => state.currentStep);
+  //const currentStep = useFormStore((state) => state.currentStep);
 
   const [isBackVisible, setBackVisible] = useState(false);
   const [isNextVisible, setNextVisible] = useState(false);
   const [isReadOnly, setReadOnly] = useState(false);
+
+  const [defaultValues, setDefaultValues] = useState<object[]>([{}]);
+  const formValues = useRef<object[]>([]);
+  const [currentStep, setCurrentStep] = useState(0);
 
   const isDirtyRef = useRef(false);
   //const [isDirty, setDirty] = useState(false);
@@ -50,24 +56,58 @@ export function EventFormProvider({ children }: { children: React.ReactNode }) {
   //const [currentStep, setCurrentStep] = useState(storedStep);
   const [formId, setFormId] = useState("");
 
+  /*const resetForm = (defaultValues: object) => {
+    currentForm?.reset();
+  };*
+*/
   const getFormData = (schema: z.ZodObject, defaultValues: object) => {
-    const state = getSessionState();
+    /* const state = getSessionState();
     const sessionData = state.localData[currentStep] as { [key: string]: any };
 
     if (isEmpty(sessionData) || !schema) {
       //setSessionFormData(defaultValues, currentStep);
       return defaultValues;
     }
-
-    const result = schema.safeParse(sessionData);
+*/
+    const result = schema.safeParse(formValues.current[currentStep]);
     //console.log(result.error);
     if (result.error) return defaultValues;
 
     return result.data;
   };
 
-  const setFormData = (data: object, id: number, step?: number) => {
-    setSessionFormData(data, step ? step : currentStep, id);
+  const setFormDefaultData = (data: object, step?: number) => {
+    const newFormDefaults = defaultValues.map((values, index) => {
+      if (index === step ? step : currentStep) {
+        return { ...values, data };
+      } else {
+        return values;
+      }
+    });
+    setDefaultValues(newFormDefaults);
+  };
+
+  const setFormData = (data: object, step?: number) => {
+    const selectedStep = step == null ? currentStep : step;
+
+    const newFormValues = formValues.current.map((values, index) => {
+      if (index === selectedStep) {
+        return { ...values, ...data };
+      } else {
+        return values;
+      }
+    });
+
+    if (formValues.current.length === 0) {
+      newFormValues.push(data);
+      //setFormValues([{ ...formValues, ...data }]);
+    } else if (selectedStep >= formValues.current.length) {
+      newFormValues.push(data);
+    }
+    formValues.current = newFormValues;
+    //setFormValues(newFormValues);
+
+    //setSessionFormData(data, step ? step : currentStep, id);
   };
 
   const isValidSchema = (schema: z.ZodObject, data: object) => {
@@ -79,11 +119,15 @@ export function EventFormProvider({ children }: { children: React.ReactNode }) {
   };
 
   const getKeyData = (step: number, key: string) => {
-    const state = getSessionState();
+    /*const state = getSessionState();
     const sessionData = state.localData[step] as { [key: string]: string | undefined };
     if (isEmpty(sessionData)) return "";
 
-    return sessionData[key];
+    return sessionData[key];*/
+    if (formValues.current.length - 1 !== step) return null;
+
+    const data = formValues.current[step] as { [key: string]: string | undefined };
+    return data[key];
   };
 
   const setKeyData = (key: string, value: string, step: number) => {
@@ -96,23 +140,29 @@ export function EventFormProvider({ children }: { children: React.ReactNode }) {
   };
 
   const getStepData = (step: number) => {
+    /*
     const state = getSessionState();
     const sessionData = state.localData[step];
     if (isEmpty(sessionData)) return {};
 
-    return sessionData;
+    return sessionData;*/
+
+    return formValues.current[step];
   };
 
   const incrementStep = (step?: number) => {
-    setSessionStep(step ? step : currentStep + 1);
+    const value = step == null ? currentStep + 1 : step;
+    setCurrentStep(value);
   };
 
   const decrementStep = (step?: number) => {
-    setSessionStep(step ? step : currentStep - 1);
+    const value = step == null ? currentStep - 1 : step;
+    setCurrentStep(value);
   };
 
   const resetForm = () => {
-    resetSessionFormData();
+    formValues.current = [];
+    //resetSessionFormData();
   };
 
   const setDirty = (value: boolean) => {
@@ -130,6 +180,8 @@ export function EventFormProvider({ children }: { children: React.ReactNode }) {
         setReadOnly,
         currentForm,
         setCurrentForm,
+        defaultValues,
+        setFormDefaultData,
         currentStep,
         //setCurrentStep,
         formId,
