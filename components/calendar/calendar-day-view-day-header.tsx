e8da12a61662def8e20ff37dd99d69df1b70fdc5
Jordan Kahtava <j.kahtava@cityssm.on.ca>
2025-10-15 15:45:19 -0400

    removed obsolete code

--- components/calendar/calendar-day-view-day-header.tsx
+++ components/calendar/calendar-day-view-day-header.tsx
@@ -12,44 +12,13 @@ export function DayViewDayHeader({ dayView }: { dayView: IDayView | undefined })
     if (dayView) push(navigateURL(dayView.dayDate, "day"));
   };
 
-  return dayView ? (
-    <Button variant={"link"} size={"sm"} onClick={handleClick}>
-      <Link href="day-view">
-        <span className="py-2 text-center text-xs font-medium text-muted-foreground">
-          {format(dayView.dayDate, "EE")}{" "}
-          <span className="ml-1 font-semibold text-foreground">{format(dayView.dayDate, "d")}</span>
-        </span>
-      </Link>
-    </Button>
-  ) : (
-    <Button variant={"link"} size={"sm"}>
+  return;
+  <Button variant={"link"} size={"sm"} onClick={handleClick}>
+    <Link href="day-view">
       <span className="py-2 text-center text-xs font-medium text-muted-foreground">
-        ERROR <span className="ml-1 font-semibold text-foreground">{format(dayView.dayDate, "d")}</span>
+        {format(dayView.dayDate, "EE")}{" "}
+        <span className="ml-1 font-semibold text-foreground">{format(dayView.dayDate, "d")}</span>
       </span>
-    </Button>
-  );
-  /*
-  return (
-    <div className="relative z-20 flex border-b">
-      <div className="w-18"></div>
-      <div className={`grid flex-1 grid-cols-${weekDays.length} divide-x border-l`}>
-        {weekDays.map((day, index) =>
-          weekDays.length > 1 ? (
-            <Button key={index} variant={"link"} size={"sm"} onClick={() => handleClick()}>
-              <Link href="day-view">
-                <span key={index} className="py-2 text-center text-xs font-medium text-muted-foreground">
-                  {format(day, "EE")} <span className="ml-1 font-semibold text-foreground">{format(day, "d")}</span>
-                </span>
-              </Link>
-            </Button>
-          ) : (
-            <span key={index} className="py-2 text-center text-xs font-medium text-muted-foreground">
-              {format(day, "EE")} <span className="ml-1 font-semibold text-foreground">{format(day, "d")}</span>
-            </span>
-          )
-        )}
-      </div>
-    </div>
-  );
-  */
+    </Link>
+  </Button>;
 }
