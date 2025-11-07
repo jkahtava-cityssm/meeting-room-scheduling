2b35a853dfd175bfd56017db32c17f36515882d8
jkahtava-cityssm <j.kahtava@cityssm.on.ca>
2025-11-06 23:40:36 -0500

    added event route for status fitler, started creating approval card list.

--- app/(private)/bookings/user-requests/page.tsx
+++ app/(private)/bookings/user-requests/page.tsx
@@ -2,87 +2,78 @@
 import { CalendarDayColumnCalendar } from "@/components/calendar/calendar-day-column-calendar";
 import { EventBlock } from "@/components/calendar/calendar-day-event-block";
 import { CalendarDayViewSkeleton } from "@/components/calendar/skeleton-calendar-day-view";
+import { Card } from "@/components/ui/card";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { useClientSession } from "@/hooks/use-client-auth";
 import { IEvent } from "@/lib/schemas/calendar";
+import { useEventQuery, useEventsByStatusQuery, useEventsQuery } from "@/services/events";
 import { useUserEventsQuery } from "@/services/users";
-import { format } from "date-fns";
+import { endOfDay, format, startOfDay } from "date-fns";
 import { redirect } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const isLoading = false;
-  const date = new Date();
-  const dayViews: { day: string }[] = [{ day: "test" }];
+	const isLoading = false;
+	const date = new Date();
+	const dayViews: { day: string }[] = [{ day: "test" }];
 
-  const { session, isPending } = useClientSession();
+	const { session, isPending } = useClientSession();
+	const startDate: Date = startOfDay(date);
+	const endDate: Date = endOfDay(date);
 
-  const [filteredEvents, setFilteredEvents] = useState<IEvent[]>([]);
-  const { data: events } = useUserEventsQuery(session?.user.id);
+	const [filteredEvents, setFilteredEvents] = useState<IEvent[]>([]);
+	const { data: events } = useEventsByStatusQuery(startDate, endDate, "1");
 
-  useEffect(() => {
-    if (!events) {
-      return;
-    }
+	useEffect(() => {
+		if (!events) {
+			return;
+		}
 
-    setFilteredEvents(events);
-  }, [events]);
+		setFilteredEvents(events);
+	}, [events]);
 
-  if (isPending) {
-    return <div>Verifying Access</div>;
-  }
+	if (isPending) {
+		return <div>Verifying Access</div>;
+	}
 
-  if (!session) {
-    //console.log("User Requests No session, redirecting to login");
-    redirect("/");
-  }
+	if (!session) {
+		//console.log("User Requests No session, redirecting to login");
+		redirect("/");
+	}
 
-  return (
-    <>
-      <div>THIS WILL BE A VIEW FOR RECEPTION TO APPROVE OR DENY BOOKINGS</div>
-      <div className="flex">
-        {isLoading ? (
-          <CalendarDayViewSkeleton />
-        ) : (
-          <div className="flex flex-1 flex-col">
-            <ScrollArea className="max-h-[50vh] md:max-h-[60vh] lg:max-h-[70vh] xl:max-h-[73vh]" type="always">
-              <div className="flex border-l">
-                {/* Day grid */}
-                <div className="relative flex-1 border-b">
-                  <div className="relative">
-                    {filteredEvents &&
-                      filteredEvents.map((event) => {
-                        return (
-                          <div
-                            key={`day-${dayViews[0].day}-block-${format(new Date(), "yyyy-MM-dd-HH-mm")}-event-`}
-                            className="absolute p-1"
-                          >
-                            <EventBlock
-                              eventBlock={{
-                                event: event,
-                                eventStyle: { top: "1px", width: "10px", left: "110px" },
-                                eventHeight: 100,
-                                groupIndex: 1,
-                                eventIndex: 1,
-                              }}
-                              heightInPixels={96}
-                            />
-                          </div>
-                        );
-                      })}
-                  </div>
-                </div>
-              </div>
-            </ScrollArea>
-          </div>
-        )}
-        <CalendarDayColumnCalendar
-          date={date}
-          isLoading={isLoading}
-          events={filteredEvents}
-          view={"day"}
-        ></CalendarDayColumnCalendar>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div>THIS WILL BE A VIEW FOR RECEPTION TO APPROVE OR DENY BOOKINGS</div>
+			<div className="flex">
+				{isLoading ? (
+					<CalendarDayViewSkeleton />
+				) : (
+					<div className="flex flex-1 flex-col">
+						<ScrollArea
+							className="max-h-[50vh] md:max-h-[60vh] lg:max-h-[70vh] xl:max-h-[73vh]"
+							type="always"
+						>
+							<div className="flex border-l">
+								{/* Day grid */}
+								<div className="relative flex-1 border-b">
+									<div className="relative">
+										{filteredEvents &&
+											filteredEvents.map((event, index) => {
+												return <Card key={index}>LLLL</Card>;
+											})}
+									</div>
+								</div>
+							</div>
+						</ScrollArea>
+					</div>
+				)}
+				<CalendarDayColumnCalendar
+					date={date}
+					isLoading={isLoading}
+					events={filteredEvents}
+					view={"day"}
+				></CalendarDayColumnCalendar>
+			</div>
+		</>
+	);
 }
--- services/events.ts
+++ services/events.ts
@@ -8,84 +8,102 @@ import { SEvent } from "@/lib/schemas/calendar";
 //const queryClient = new QueryClient();
 
 export const useEventsQuery = (startDate: Date, endDate: Date, userId?: string, enabled: boolean = true) =>
-  useQuery({
-    queryKey: ["events", formatDate(startDate), formatDate(endDate), userId],
-    queryFn: async () =>
-      fetchGET("/api/events", {
-        startdate: formatDate(startDate),
-        enddate: formatDate(endDate),
-        userId: userId,
-      }).then((result) => {
-        const parsedResult = z.array(SEvent).safeParse(result.data);
+	useQuery({
+		queryKey: ["events", formatDate(startDate), formatDate(endDate), userId],
+		queryFn: async () =>
+			fetchGET("/api/events", {
+				startdate: formatDate(startDate),
+				enddate: formatDate(endDate),
+				userId: userId,
+			}).then(result => {
+				const parsedResult = z.array(SEvent).safeParse(result.data);
 
-        if (!parsedResult.success) throw new Error("Invalid event data");
+				if (!parsedResult.success) throw new Error("Invalid event data");
 
-        return parsedResult.data;
-      }),
-    enabled: enabled,
-  });
+				return parsedResult.data;
+			}),
+		enabled: enabled,
+	});
+
+export const useEventsByStatusQuery = (startDate: Date, endDate: Date, statusId?: string, enabled: boolean = true) =>
+	useQuery({
+		queryKey: ["event_status", formatDate(startDate), formatDate(endDate), statusId],
+		queryFn: async () =>
+			fetchGET("/api/events/status", {
+				startdate: formatDate(startDate),
+				enddate: formatDate(endDate),
+				statusId: statusId,
+			}).then(result => {
+				const parsedResult = z.array(SEvent).safeParse(result.data);
+
+				if (!parsedResult.success) throw new Error("Invalid event data");
+
+				return parsedResult.data;
+			}),
+		enabled: enabled,
+	});
 
 const formatDate = (date: Date) => {
-  return formatISO(date);
+	return formatISO(date);
 };
 
 export const useEventQuery = (eventId: number | undefined, enabled: boolean = true) =>
-  useQuery({
-    queryKey: ["event", eventId],
-    queryFn: async () =>
-      fetchGET(`/api/events/${eventId}`).then((result) => {
-        const parsedResult = z.array(SEvent).safeParse(result.data);
+	useQuery({
+		queryKey: ["event", eventId],
+		queryFn: async () =>
+			fetchGET(`/api/events/${eventId}`).then(result => {
+				const parsedResult = z.array(SEvent).safeParse(result.data);
 
-        if (!parsedResult.success) throw new Error("Invalid event data");
+				if (!parsedResult.success) throw new Error("Invalid event data");
 
-        return parsedResult.data[0];
-      }),
-    enabled: enabled && eventId !== undefined,
-    gcTime: 0,
-    staleTime: 0,
-  });
+				return parsedResult.data[0];
+			}),
+		enabled: enabled && eventId !== undefined,
+		gcTime: 0,
+		staleTime: 0,
+	});
 
 type eventObject = {
-  eventId?: number;
-  roomId: number;
-  startDate: string;
-  endDate: string;
-  title: string;
-  description?: string;
-  recurrenceId?: number;
+	eventId?: number;
+	roomId: number;
+	startDate: string;
+	endDate: string;
+	title: string;
+	description?: string;
+	recurrenceId?: number;
 };
 
 type ruleObject = {
-  rule: string;
-  ruleStartDate: string;
-  ruleEndDate: string;
+	rule: string;
+	ruleStartDate: string;
+	ruleEndDate: string;
 };
 
 type mutationObject = {
-  eventData: eventObject;
-  ruleData: ruleObject | null;
+	eventData: eventObject;
+	ruleData: ruleObject | null;
 };
 
 export const useEventsMutationUpsert = () => {
-  const queryClient = useQueryClient();
-  return useMutation({
-    mutationFn: async (data: mutationObject) => fetchPUT(`/api/events`, data),
-    onSuccess: (response) => {
-      // Invalidate and refetch
-      queryClient.invalidateQueries({ queryKey: ["events"] });
-      queryClient.invalidateQueries({ queryKey: ["event", response.data.eventId] });
-    },
-  });
+	const queryClient = useQueryClient();
+	return useMutation({
+		mutationFn: async (data: mutationObject) => fetchPUT(`/api/events`, data),
+		onSuccess: response => {
+			// Invalidate and refetch
+			queryClient.invalidateQueries({ queryKey: ["events"] });
+			queryClient.invalidateQueries({ queryKey: ["event", response.data.eventId] });
+		},
+	});
 };
 
 export const useEventsMutationDelete = () => {
-  const queryClient = useQueryClient();
-  return useMutation({
-    mutationFn: async (eventId: number) => fetchDELETE(`/api/events/${eventId}`),
-    onSuccess: () => {
-      // Invalidate and refetch
-      queryClient.invalidateQueries({ queryKey: ["events"] });
-      //queryClient.invalidateQueries({ queryKey: ["event", response.data.eventId] });
-    },
-  });
+	const queryClient = useQueryClient();
+	return useMutation({
+		mutationFn: async (eventId: number) => fetchDELETE(`/api/events/${eventId}`),
+		onSuccess: () => {
+			// Invalidate and refetch
+			queryClient.invalidateQueries({ queryKey: ["events"] });
+			//queryClient.invalidateQueries({ queryKey: ["event", response.data.eventId] });
+		},
+	});
 };
