97434e68a9067322881735a2410d2c782385741f
Jordan Kahtava <j.kahtava@cityssm.on.ca>
2025-12-02 12:48:57 -0500

    added status/count to collect the total events with a given statusId

---
+++ app/api/events/status/counts/route.ts
@@ -0,0 +1,50 @@
+import { prisma } from "@/prisma";
+
+import { NextRequest } from "next/server";
+
+import { UTCDate } from "@date-fns/utc";
+
+import { BadRequestMessage, InternalServerErrorMessage, SuccessMessage } from "@/lib/api-helpers";
+import { guardRoute } from "@/lib/api-guard";
+import { format } from "date-fns";
+
+export async function GET(request: NextRequest) {
+  return guardRoute(
+    request,
+    { type: "role", role: "Any" },
+
+    async (userId, roles) => {
+      const searchParams = request.nextUrl.searchParams;
+
+      const startDateParam = searchParams.get("startdate");
+      const endDateParam = searchParams.get("enddate");
+      const statusId = searchParams.get("statusId");
+
+      if (!statusId) {
+        return BadRequestMessage();
+      }
+
+      const timeClause =
+        startDateParam && endDateParam
+          ? {
+              createdAt: { lte: new UTCDate(endDateParam), gte: new UTCDate(startDateParam) },
+            }
+          : {};
+
+      const whereClause: import("@prisma/client").Prisma.EventWhereInput = {
+        AND: [timeClause, { statusId: Number(statusId) }],
+      };
+      const countEvents = await prisma.event.count({
+        where: whereClause,
+      });
+
+      if (!countEvents) {
+        return InternalServerErrorMessage();
+      }
+      //console.log("###########################################");
+      //console.log("COUNT RAN: ", format(new Date(), "PPP @ p"));
+      //console.log("###########################################");
+      return SuccessMessage("Collected Total Events", { total: countEvents });
+    }
+  );
+}
