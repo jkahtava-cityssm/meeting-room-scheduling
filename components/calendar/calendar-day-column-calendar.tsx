87871f853b69fc824cc3e5d32533239d1cb389fe
Jordan Kahtava <j.kahtava@cityssm.on.ca>
2025-12-02 12:56:52 -0500

    renamed Calendar to DayPicker to try and keep consistency, moved component out of UI to keep the UI folder specific to shadcn or other UI components.

--- components/calendar/calendar-day-column-calendar.tsx
+++ components/calendar/calendar-day-column-calendar.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { IEvent } from "@/lib/schemas/calendar";
-import { SingleCalendar } from "../ui/single-calendar-day";
+import { DayPicker } from "../ui/day-picker";
 import { Skeleton } from "../ui/skeleton";
 
 import { addYears } from "date-fns";
@@ -11,66 +11,62 @@ import { useRouter } from "next/navigation";
 import { CalendarDayColumnCurrentEvents } from "./calendar-day-column-current-events";
 
 export function CalendarDayColumnCalendar({
-	date,
-	isLoading,
-	events,
-	view,
+  date,
+  isLoading,
+  events,
+  view,
 }: {
-	date: Date;
-	isLoading: boolean;
-	events: IEvent[];
-	view: TCalendarView;
+  date: Date;
+  isLoading: boolean;
+  events: IEvent[];
+  view: TCalendarView;
 }) {
-	const [calendarDate, setCalendarDate] = useState(date);
+  const [calendarDate, setCalendarDate] = useState(date);
 
-	const { push } = useRouter();
+  const { push } = useRouter();
 
-	/*const [currentMonth, setCurrentMonth] = React.useState<Date | undefined>(
+  /*const [currentMonth, setCurrentMonth] = React.useState<Date | undefined>(
     selected instanceof Date ? selected : undefined
   );*/
 
-	const handleToday = () => {
-		setCalendarDate(new Date());
+  const handleToday = () => {
+    setCalendarDate(new Date());
 
-		push(navigateURL(new Date(), view));
-	};
+    push(navigateURL(new Date(), view));
+  };
 
-	return (
-		<div className="hidden w-74 divide-y border-l md:block">
-			<SingleCalendar
-				className="mx-auto w-fit"
-				mode="single"
-				selected={date}
-				onSelect={value => {
-					push(navigateURL(value, view));
-				}}
-				month={calendarDate}
-				onMonthChange={setCalendarDate}
-				fixedWeeks={true}
-				required
-				onToday={handleToday}
-				view={view}
-				startMonth={addYears(date, -25)}
-				endMonth={addYears(date, 25)}
-			/>
-			{isLoading ? (
-				<div className="flex-1 space-y-2">
-					<div className="flex items-start gap-1 px-4 pt-4">
-						<Skeleton className="w-full h-4"></Skeleton>
-					</div>
-					<div className="flex items-start gap-1 px-4 ">
-						<Skeleton className="w-full h-4"></Skeleton>
-					</div>
-					<div className="flex items-start gap-1 px-4 ">
-						<Skeleton className="w-full h-4"></Skeleton>
-					</div>
-				</div>
-			) : (
-				<CalendarDayColumnCurrentEvents
-					date={date}
-					events={events}
-				></CalendarDayColumnCurrentEvents>
-			)}
-		</div>
-	);
+  return (
+    <div className="hidden w-74 divide-y border-l md:block">
+      <DayPicker
+        className="mx-auto w-fit"
+        mode="single"
+        selected={date}
+        onSelect={(value) => {
+          push(navigateURL(value, view));
+        }}
+        month={calendarDate}
+        onMonthChange={setCalendarDate}
+        fixedWeeks={true}
+        required
+        onToday={handleToday}
+        startMonth={addYears(date, -25)}
+        endMonth={addYears(date, 25)}
+      />
+      {isLoading ? (
+        <div className="flex-1 space-y-2">
+          <div className="flex items-start gap-1 px-4 pt-4">
+            <Skeleton className="w-full h-4"></Skeleton>
+          </div>
+          <div className="flex items-start gap-1 px-4 ">
+            <Skeleton className="w-full h-4"></Skeleton>
+          </div>
+          <div className="flex items-start gap-1 px-4 ">
+            <Skeleton className="w-full h-4"></Skeleton>
+          </div>
+        </div>
+      ) : (
+        <CalendarDayColumnCurrentEvents date={date} events={events}></CalendarDayColumnCurrentEvents>
+      )}
+    </div>
+  );
 }
