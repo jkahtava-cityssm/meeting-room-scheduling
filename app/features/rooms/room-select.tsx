3eb838a8f672b317ee53b13ef47d4a6fd693d934
jkahtava-cityssm <j.kahtava@cityssm.on.ca>
2026-01-20 17:42:47 -0500

    added error state to room selection

--- app/features/rooms/room-select.tsx
+++ app/features/rooms/room-select.tsx
@@ -4,30 +4,31 @@ import { TColors } from "@/lib/types";
 import { useRoomsQuery } from "@/lib/services/rooms";
 
 export function RoomSelect({
-  selectedRoomId,
-  includeAllOption = true,
-  onRoomChange,
+	selectedRoomId,
+	includeAllOption = true,
+	onRoomChange,
 }: {
-  selectedRoomId: string;
-  includeAllOption: boolean;
-  onRoomChange: (value: string) => void;
+	selectedRoomId: string;
+	includeAllOption: boolean;
+	onRoomChange: (value: string) => void;
 }) {
-  const { isPending, data } = useRoomsQuery(includeAllOption);
+	const { isPending, data, error } = useRoomsQuery(includeAllOption);
 
-  return (
-    <GenericSelect
-      list={data}
-      selectedValue={selectedRoomId}
-      isLoading={isPending}
-      loadingLabel="Collecting Rooms"
-      onChange={(value) => {
-        onRoomChange(value);
-      }}
-      getId={(room) => room.roomId.toString()}
-      getLabel={(room) => room.name}
-      getColor={(room) => room.color as TColors}
-      getIcon={(room) => room.icon as IconName}
-      className="min-w-60"
-    />
-  );
+	return (
+		<GenericSelect
+			list={data}
+			selectedValue={selectedRoomId}
+			isLoading={isPending}
+			isError={!!error}
+			loadingLabel={error ? "Error: Collecting Rooms" : "Collecting Rooms"}
+			onChange={value => {
+				onRoomChange(value);
+			}}
+			getId={room => room.roomId.toString()}
+			getLabel={room => room.name}
+			getColor={room => room.color as TColors}
+			getIcon={room => room.icon as IconName}
+			className="min-w-60"
+		/>
+	);
 }
