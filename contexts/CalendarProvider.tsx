309d953fba23715e042ac1d4daeab241a161ea49
jkahtava-cityssm <j.kahtava@cityssm.on.ca>
2026-01-25 20:18:02 -0500

    removed working hours because we dont care about them, if the you can book outside of working hours and you are not supposed to then those hours shouldnt be visible anyways.

--- contexts/CalendarProvider.tsx
+++ contexts/CalendarProvider.tsx
@@ -11,76 +11,58 @@ import { useRoomsQuery } from "@/lib/services/rooms";
 import { IRoom } from "@/lib/schemas/calendar";
 
 interface ICalendarContext {
-  selectedDate: Date;
-  setSelectedDate: (date: Date | undefined) => void;
-  isHeaderLoading: boolean;
-  setIsHeaderLoading: (value: boolean) => void;
-  totalEvents: number;
-  setTotalEvents: (total: number) => void;
-  selectedRoomId: string;
-  setSelectedRoomId: (roomId: string) => void;
-  workingHours: TWorkingHours;
-  setWorkingHours: Dispatch<SetStateAction<TWorkingHours>>;
-  visibleRooms: IRoom[] | undefined;
-  visibleHours: TVisibleHours;
-  interval: number;
-  //setVisibleHours: Dispatch<SetStateAction<TVisibleHours>>;
+	selectedDate: Date;
+	setSelectedDate: (date: Date | undefined) => void;
+	isHeaderLoading: boolean;
+	setIsHeaderLoading: (value: boolean) => void;
+	totalEvents: number;
+	setTotalEvents: (total: number) => void;
+	selectedRoomId: string;
+	setSelectedRoomId: (roomId: string) => void;
+	visibleRooms: IRoom[] | undefined;
+	visibleHours: TVisibleHours;
+	interval: number;
+	//setVisibleHours: Dispatch<SetStateAction<TVisibleHours>>;
 }
 
 const CalendarContext = createContext({} as ICalendarContext);
 
-const WORKING_HOURS = {
-  0: { from: 0, to: 0 },
-  1: { from: 8, to: 17 },
-  2: { from: 8, to: 17 },
-  3: { from: 8, to: 17 },
-  4: { from: 8, to: 17 },
-  5: { from: 8, to: 17 },
-  6: { from: 8, to: 12 },
-};
-
 export function CalendarProvider({ children }: { children: React.ReactNode }) {
-  //const [visibleHours, setVisibleHours] = useState<TVisibleHours>(VISIBLE_HOURS);
-  const [workingHours, setWorkingHours] = useState<TWorkingHours>(WORKING_HOURS);
+	const { data: configurationData } = usePublicConfiguration();
+	const { data: visibleRooms } = useRoomsQuery();
 
-  const { data: configurationData } = usePublicConfiguration();
-  const { data: visibleRooms } = useRoomsQuery();
+	const visibleHours: TVisibleHours = configurationData ? configurationData.hours : VISIBLE_HOURS;
+	const interval = configurationData ? configurationData.interval : 30;
 
-  const visibleHours: TVisibleHours = configurationData ? configurationData.hours : VISIBLE_HOURS;
-  const interval = configurationData ? configurationData.interval : 30;
+	const [isHeaderLoading, setIsHeaderLoading] = useState(true);
+	const [totalEvents, setTotalEvents] = useState(0);
+	const [selectedDate, setSelectedDate] = useState(new Date());
+	const [selectedRoomId, setSelectedRoomId] = useState<string>("-1");
 
-  const [isHeaderLoading, setIsHeaderLoading] = useState(true);
-  const [totalEvents, setTotalEvents] = useState(0);
-  const [selectedDate, setSelectedDate] = useState(new Date());
-  const [selectedRoomId, setSelectedRoomId] = useState<string>("-1");
+	const handleSelectDate = (date: Date | undefined) => {
+		if (!date) return;
+		setSelectedDate(date);
+	};
 
-  const handleSelectDate = (date: Date | undefined) => {
-    if (!date) return;
-    setSelectedDate(date);
-  };
-
-  return (
-    <CalendarContext.Provider
-      value={{
-        isHeaderLoading,
-        setIsHeaderLoading,
-        totalEvents,
-        setTotalEvents,
-        selectedDate,
-        setSelectedDate: handleSelectDate,
-        selectedRoomId,
-        setSelectedRoomId,
-        visibleRooms,
-        visibleHours,
-        interval,
-        //setVisibleHours,
-        workingHours,
-        setWorkingHours,
-      }}
-    >
-      {children}
-    </CalendarContext.Provider>
-  );
+	return (
+		<CalendarContext.Provider
+			value={{
+				isHeaderLoading,
+				setIsHeaderLoading,
+				totalEvents,
+				setTotalEvents,
+				selectedDate,
+				setSelectedDate: handleSelectDate,
+				selectedRoomId,
+				setSelectedRoomId,
+				visibleRooms,
+				visibleHours,
+				interval,
+			}}
+		>
+			{children}
+		</CalendarContext.Provider>
+	);
 }
 
 export function useCalendar(): ICalendarContext {
