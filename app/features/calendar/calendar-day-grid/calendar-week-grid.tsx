e85a464662ba3a004f8af80ecc284b3924b4adcb
Jordan Kahtava <j.kahtava@cityssm.on.ca>
2026-02-02 10:12:26 -0500

    adjusted imports for week view to use Private Scroll Column because it was renamed.

--- app/features/calendar/calendar-day-grid/calendar-week-grid.tsx
+++ app/features/calendar/calendar-day-grid/calendar-week-grid.tsx
@@ -12,58 +12,58 @@ import { CalendarDayViewSkeleton } from "@/app/features/calendar/view-day/skelet
 
 import { useSharedEventDrawer } from "../../event-drawer/shared-event-drawer-context";
 import { CalendarScrollContainerPrivate } from "../components/calendar-scroll-container";
-import { CalendarScrollColumn } from "../components/calendar-scroll-column";
+import { CalendarScrollColumnPrivate } from "../components/calendar-scroll-column";
 
 export const WeeklyBlocks = React.memo(function WeeklyBlocks({
-	isLoading,
-	selectedRoomId,
-	visibleRooms,
-	roomBlocks,
+  isLoading,
+  selectedRoomId,
+  visibleRooms,
+  roomBlocks,
 }: {
-	isLoading: boolean;
-	selectedRoomId: number;
-	visibleRooms: IRoom[] | undefined;
-	roomBlocks: Map<string, IBlock[]> | undefined;
+  isLoading: boolean;
+  selectedRoomId: number;
+  visibleRooms: IRoom[] | undefined;
+  roomBlocks: Map<string, IBlock[]> | undefined;
 }) {
-	const { hours, currentDate, interval, userId } = useCalendarDayGrid();
-	const roomsToRender = React.useMemo(
-		() =>
-			visibleRooms
-				?.filter(room => selectedRoomId === -1 || room.roomId === selectedRoomId)
-				.map(room => {
-					const blocks = roomBlocks?.get(String(room.roomId)) ?? [];
-					return { roomId: room.roomId, roomName: room.name, blocks };
-				}),
-		[visibleRooms, selectedRoomId, roomBlocks],
-	);
+  const { hours, currentDate, interval, userId } = useCalendarDayGrid();
+  const roomsToRender = React.useMemo(
+    () =>
+      visibleRooms
+        ?.filter((room) => selectedRoomId === -1 || room.roomId === selectedRoomId)
+        .map((room) => {
+          const blocks = roomBlocks?.get(String(room.roomId)) ?? [];
+          return { roomId: room.roomId, roomName: room.name, blocks };
+        }),
+    [visibleRooms, selectedRoomId, roomBlocks],
+  );
 
-	const lastRoomId = roomsToRender?.length ? roomsToRender[roomsToRender.length - 1].roomId : undefined;
+  const lastRoomId = roomsToRender?.length ? roomsToRender[roomsToRender.length - 1].roomId : undefined;
 
-	const isMounting = !visibleRooms || !roomBlocks || !hours;
+  const isMounting = !visibleRooms || !roomBlocks || !hours;
 
-	return (
-		<CalendarScrollContainerPrivate
-			isLoading={isLoading}
-			hours={hours || []}
-			isMounting={isMounting}
-			skeleton={<CalendarDayViewSkeleton hours={hours} />}
-		>
-			{roomsToRender?.map(room => {
-				return (
-					<CalendarScrollColumn
-						key={room.roomId}
-						loadingBlocks={isLoading}
-						title={room.roomName}
-						interval={interval}
-						roomId={room.roomId}
-						userId={userId}
-						hours={hours || []}
-						eventBlocks={room.blocks || []}
-						isLastColumn={room.roomId === lastRoomId}
-						currentDate={currentDate}
-					/>
-				);
-			})}
-		</CalendarScrollContainerPrivate>
-	);
+  return (
+    <CalendarScrollContainerPrivate
+      isLoading={isLoading}
+      hours={hours || []}
+      isMounting={isMounting}
+      skeleton={<CalendarDayViewSkeleton hours={hours} />}
+    >
+      {roomsToRender?.map((room) => {
+        return (
+          <CalendarScrollColumnPrivate
+            key={room.roomId}
+            loadingBlocks={isLoading}
+            title={room.roomName}
+            interval={interval}
+            roomId={room.roomId}
+            userId={userId}
+            hours={hours || []}
+            eventBlocks={room.blocks || []}
+            isLastColumn={room.roomId === lastRoomId}
+            currentDate={currentDate}
+          />
+        );
+      })}
+    </CalendarScrollContainerPrivate>
+  );
 });
