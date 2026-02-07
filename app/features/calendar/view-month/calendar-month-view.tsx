70ce7d96e936cf1e0f06b8f168c35f18d4f1036e
jkahtava-cityssm <j.kahtava@cityssm.on.ca>
2026-02-07 15:25:44 -0500

    set month view to use new generic worker, and adjusted dates references from normal dates to isostring.

--- app/features/calendar/view-month/calendar-month-view.tsx
+++ app/features/calendar/view-month/calendar-month-view.tsx
@@ -15,129 +15,31 @@ import { getDaysInView } from "@/lib/helpers";
 import { useEventsQuery } from "@/lib/services/events";
 import { TVisibleHours } from "@/lib/types";
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
-
-export interface IMonthProcessData {
-	events: IEvent[];
-	selectedDate: Date;
-	selectedRoomId: string;
-	multiDayEventsAtTop: boolean;
-	visibleHours: TVisibleHours;
-}
-
-export interface IMonthResponseData {
-	totalEvents: number;
-	dayViews: IDayView[];
-	weekViews: IWeekView[];
-}
-
-export interface IWeekView {
-	week: number;
-	maxDailyEvents: number;
-	dayViews: IDayView[];
-}
-
-export interface IDayView {
-	day: number;
-	dayDate: Date;
-	isToday: boolean;
-	isSunday: boolean;
-	isCurrentMonth: boolean;
-	eventRecords: IEventView[];
-}
-
-export interface IEventView {
-	index: number;
-	position: "none" | "middle" | "first" | "last";
-	event: IEvent | undefined;
-}
+import { usePrivateCalendarEvents } from "../webworkers/use-calendar-private-events";
 
 const WEEK_DAYS = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
 export function CalendarMonthView({ date, userId }: { date: Date; userId?: string }) {
-	//const startDate: Date = startOfMonth(date);
-	//const endDate: Date = endOfMonth(date);
-
-	const { startDate, endDate } = getDaysInView(date);
 	const { visibleHours, selectedRoomId, setTotalEvents, setIsHeaderLoading } = usePrivateCalendar();
 
-	const workerRef = useRef<Worker | null>(null);
-
-	const [weekViews, setWeekViews] = useState<IWeekView[]>([]);
-
-	const [isLoading, setLoading] = useState(true);
-	const [isRefreshed, setRefreshed] = useState(false);
-
-	/*const { data: events, isLoading: isPending } = useSWR<IEvent[]>(
-    `/api/events?startdate=${startDate.toISOString()}&enddate=${endDate.toISOString()}`
-  );*/
-
-	const { isPending, data: events } = useEventsQuery(startDate, endDate, userId);
+	const { result, isLoading } = usePrivateCalendarEvents("MONTH", date, visibleHours, userId, selectedRoomId);
 
 	useEffect(() => {
-		//The Workerthread needs to be recreated when we navigate back to the page if the params havent changed.
-		//nextjs cache's the route so this is my temporary fix
-		setRefreshed(true);
-	}, []);
-
-	useEffect(() => {
-		setLoading(true);
-	}, [date]);
-
-	useEffect(() => {
-		//This is mostly as an example for myself, technically this processing should likely be done on the server side.
-		//But this example will come in handy for other applications
-
-		if (workerRef.current) {
-			return;
-		}
-
-		const newWorker = new Worker(new URL("../webworkers/calendar-month-webworker.ts", import.meta.url));
-
-		newWorker.onmessage = (event: MessageEvent<IMonthResponseData>) => {
-			setWeekViews(event.data.weekViews);
-			setTotalEvents(event.data.totalEvents);
-			setIsHeaderLoading(false);
-			setLoading(false);
-		};
-
-		workerRef.current = newWorker;
-
-		return () => {
-			if (workerRef.current) {
-				workerRef.current.terminate();
-				workerRef.current = null;
-			}
-		};
-	}, [date, setIsHeaderLoading, setTotalEvents]);
-
-	useEffect(() => {
-		if (!events) {
-			return;
-		}
-
-		if (workerRef.current) {
-			const data: IMonthProcessData = {
-				events: events,
-				selectedDate: date,
-				selectedRoomId: selectedRoomId,
-				multiDayEventsAtTop: true,
-				visibleHours: visibleHours,
-			};
-			//setLoading(true);
+		if (isLoading) {
 			setIsHeaderLoading(true);
-
-			workerRef.current.postMessage(data);
 		}
-	}, [events, date, selectedRoomId, isRefreshed, setIsHeaderLoading, visibleHours]);
 
-	if (isLoading || isPending) {
+		if (result && !isLoading) {
+			setIsHeaderLoading(false);
+			setTotalEvents(result.totalEvents);
+		}
+	}, [isLoading, result, setIsHeaderLoading, setTotalEvents]);
+
+	const isMounting = !result || isLoading;
+	console.log(result, isLoading, isMounting);
+	if (isMounting) {
 		return <MonthViewDayCellSkeleton date={date} />;
 	}
-	//134
-	//24
-	//94
-	//26
-
 	return (
 		<>
 			<div
@@ -154,7 +56,7 @@ export function CalendarMonthView({ date, userId }: { date: Date; userId?: strin
 				))}
 			</div>
 			<div>
-				{weekViews.map(week => {
+				{result.data.weekViews.map(week => {
 					return (
 						<div key={`week-${week.week}`}>
 							<div
@@ -179,7 +81,7 @@ export function CalendarMonthView({ date, userId }: { date: Date; userId?: strin
 										{week.dayViews.map(day => {
 											return (
 												<MonthViewDayEvents
-													key={day.dayDate.toISOString()}
+													key={day.dayDate}
 													dayRecord={day}
 													userId={userId}
 												/>
