0c81d3caaa8292826dceff34137e4ae9ebe9626a
jkahtava-cityssm <j.kahtava@cityssm.on.ca>
2026-02-07 21:40:52 -0500

    added some loading states when a page is hydrating.

--- app/(private)/availability/page.tsx
+++ app/(private)/availability/page.tsx
@@ -2,19 +2,18 @@
 
 import { CalendarPublicView } from "@/app/features/calendar/view-public/public-view";
 import { useSidebar } from "@/components/ui/sidebar";
+import { Skeleton } from "@/components/ui/skeleton";
 import { CalendarProviderPublic } from "@/contexts/CalendarProviderPublic";
+import { LoaderCircle } from "lucide-react";
 
 import { Suspense } from "react";
 
 export default function Availability() {
-	const { open, openMobile, isMobile } = useSidebar();
 	return (
 		<div className="overflow-hidden min-w-92 flex flex-1 flex-col min-h-0">
-			<Suspense fallback={<>...Loading</>}>
-				<CalendarProviderPublic>
-					<CalendarPublicView sideBarOpen={open && !isMobile}></CalendarPublicView>
-				</CalendarProviderPublic>
-			</Suspense>
+			<CalendarProviderPublic>
+				<CalendarPublicView></CalendarPublicView>
+			</CalendarProviderPublic>
 		</div>
 	);
 }
