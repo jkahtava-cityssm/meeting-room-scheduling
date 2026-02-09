a00809a478ce68e20b44f09d58011c54bb6dc10c
Jordan Kahtava <j.kahtava@cityssm.on.ca>
2026-02-09 09:29:48 -0500

    updated year to use generic worker.

--- app/features/calendar/view-year/calendar-year-view.tsx
+++ app/features/calendar/view-year/calendar-year-view.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { endOfYear, startOfYear } from "date-fns";
 
 import { usePrivateCalendar } from "@/contexts/CalendarProviderPrivate";
@@ -9,129 +9,50 @@ import { IEvent } from "@/lib/schemas/calendar";
 import { YearViewSkeleton } from "./skeleton-calendar-year-view";
 import { TVisibleHours } from "@/lib/types";
 import { useEventsQuery } from "@/lib/services/events";
-
-export interface IMonthView {
-	month: number;
-	monthDate: Date;
-	monthName: string;
-	days: IDayView[];
-}
-
-export interface IDayView {
-	day: number;
-	dayDate: Date;
-	isBlank: boolean;
-	isToday: boolean;
-	dayEvents: IEvent[];
-}
-
-export interface IYearProcessData {
-	eventList: IEvent[];
-	selectedDate: Date;
-	selectedRoomId: string;
-	visibleHours: TVisibleHours;
-}
-
-export interface IYearResponseData {
-	totalEvents: number;
-	monthsViews: IMonthView[];
-}
+import { usePrivateCalendarEvents } from "../webworkers/use-calendar-private-events";
 
 export function CalendarYearView({ date, userId }: { date: Date; userId?: string }) {
-	const { selectedRoomId, visibleHours, setIsHeaderLoading, setTotalEvents } = usePrivateCalendar();
+  const { interval, visibleHours, visibleRooms, selectedRoomId, setIsHeaderLoading, setTotalEvents } =
+    usePrivateCalendar();
 
-	const workerRef = useRef<Worker | null>(null);
-	const [monthViews, setMonthViews] = useState<IMonthView[]>([]);
+  const roomIds = useMemo(
+    () => (visibleRooms ? visibleRooms.map((room) => room.roomId.toString()) : []),
+    [visibleRooms],
+  );
 
-	const [isLoading, setLoading] = useState(true);
-	const [isRefreshed, setRefreshed] = useState(false);
+  const { result, isLoading } = usePrivateCalendarEvents("YEAR", date, visibleHours, userId, selectedRoomId);
 
-	const startDate: Date = startOfYear(date);
-	const endDate: Date = endOfYear(date);
+  useEffect(() => {
+    if (isLoading) {
+      setIsHeaderLoading(true);
+    }
 
-	//console.log(formatISO(startDate, { representation: "date" }));
-	/*const { data: events } = useSWR<IEvent[]>(
-    `/api/events?startdate=${formatISO(startDate, { representation: "date" })}&enddate=${formatISO(endDate, {
-      representation: "date",
-    })}`
-  );*/
+    if (result && !isLoading) {
+      setIsHeaderLoading(false);
+    }
+  }, [isLoading, result, setIsHeaderLoading, setTotalEvents]);
 
-	const { data: events } = useEventsQuery(startDate, endDate, userId);
+  useEffect(() => {
+    if (!result) return;
 
-	/*const { data: recurringEvents } = useSWR<IEvent[]>(
-    `/api/recurrences?startdate=${startDate.toISOString()}&enddate=${endDate.toISOString()}`
-  );*/
+    setTotalEvents(result.totalEvents);
+  }, [result, setTotalEvents]);
 
-	useEffect(() => {
-		//The Workerthread needs to be recreated when we navigate back to the page if the params havent changed.
-		//nextjs cache's the route so this is my temporary fix
-		setRefreshed(true);
-	}, []);
+  const isMounting = !visibleRooms || !result;
 
-	useEffect(() => {
-		setLoading(true);
-	}, [date]);
+  if (isMounting) {
+    return <YearViewSkeleton date={date}></YearViewSkeleton>;
+  }
 
-	useEffect(() => {
-		//This is mostly as an example for myself, technically this processing should likely be done on the server side.
-		//But this example will come in handy for other applications
-		const newWorker = new Worker(new URL("../webworkers/calendar-year-webworker.ts", import.meta.url));
-
-		newWorker.onmessage = (event: MessageEvent<IYearResponseData>) => {
-			setMonthViews(event.data.monthsViews);
-			setTotalEvents(event.data.totalEvents);
-			setIsHeaderLoading(false);
-			setLoading(false);
-		};
-
-		workerRef.current = newWorker;
-
-		return () => {
-			if (workerRef.current) {
-				workerRef.current.terminate();
-				workerRef.current = null;
-			}
-		};
-	}, [date, setIsHeaderLoading, setTotalEvents]);
-
-	useEffect(() => {
-		if (!events) {
-			return;
-		}
-
-		if (workerRef.current) {
-			const data: IYearProcessData = {
-				eventList: events,
-				selectedDate: date,
-				selectedRoomId: selectedRoomId,
-
-				visibleHours: visibleHours,
-			};
-			//setLoading(true);
-			setIsHeaderLoading(true);
-
-			workerRef.current.postMessage(data);
-		}
-	}, [events, date, selectedRoomId, visibleHours, isRefreshed, setIsHeaderLoading]);
-
-	if (isLoading) {
-		return <YearViewSkeleton date={date}></YearViewSkeleton>;
-	}
-
-	return (
-		<>
-			<div className="p-4">
-				<div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-					{monthViews.map(month => {
-						return (
-							<YearViewMonth
-								key={month.month.toString()}
-								month={month}
-							/>
-						);
-					})}
-				</div>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="p-4">
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          {result?.data.monthViews.map((month) => {
+            return <YearViewMonth key={month.month.toString()} month={month} />;
+          })}
+        </div>
+      </div>
+    </>
+  );
 }
