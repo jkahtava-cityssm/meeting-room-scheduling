2d074500161d5b2b7bde74cfc5be6adf980658b0
Jordan Kahtava <j.kahtava@cityssm.on.ca>
2026-02-10 15:22:39 -0500

    replaced div with button for event block, in preparation for swapping to shared Drawer.

--- app/features/calendar/components/calendar-scroll-private-event-block.tsx
+++ app/features/calendar/components/calendar-scroll-private-event-block.tsx
@@ -6,6 +6,8 @@ import { TColors } from "@/lib/types";
 import EventDrawer from "@/app/features/event-drawer/event-drawer";
 import { sharedColorVariants } from "@/lib/theme/colorVariants";
 import { IEventBlock } from "../webworkers/generic-webworker";
+import { ButtonHTMLAttributes, forwardRef } from "react";
+import { cn } from "@/lib/utils";
 
 const EventCard = cva(
   "flex select-none flex-col gap-0.5 truncate whitespace-nowrap rounded-md border px-1.5 py-0.5 text-xs focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring",
@@ -19,15 +21,17 @@ const EventCard = cva(
   },
 );
 
-export function GridEventBlock({
-  eventBlock,
-  heightInPixels,
-  userId,
-}: {
+interface MonthEventBadgeProps extends Omit<ButtonHTMLAttributes<HTMLButtonElement>, "color"> {
   eventBlock: IEventBlock;
   heightInPixels: number;
+  editEventAllowed: boolean;
   userId?: string;
-}) {
+}
+
+export const GridEventBlock = forwardRef<HTMLButtonElement, MonthEventBadgeProps>(function GridEventBlock(
+  { eventBlock, heightInPixels, editEventAllowed, userId, ...buttonProps },
+  ref,
+) {
   if (!eventBlock?.event) {
     return;
   }
@@ -44,14 +48,17 @@ export function GridEventBlock({
 
   return (
     <EventDrawer event={eventBlock.event} userId={userId}>
-      <div
-        role="button"
+      <button
+        type="button"
+        ref={ref}
         tabIndex={0}
-        className={EventCardClasses}
+        className={cn("w-full h-full", EventCardClasses)}
         style={{
           height: `${heightInPixels}px`,
         }}
         onKeyDown={handleKeyDown}
+        aria-label={buttonProps["aria-label"] ?? eventBlock.event.title}
+        {...buttonProps}
       >
         <div className="flex items-center gap-1.5 ">
           <p className="truncate font-semibold">{eventBlock.event.title}</p>
@@ -61,7 +68,7 @@ export function GridEventBlock({
             {format(eventBlock.event.startDate, "h:mm a")} - {format(eventBlock.event.endDate, "h:mm a")}
           </p>
         </div>
-      </div>
+      </button>
     </EventDrawer>
   );
-}
+});
