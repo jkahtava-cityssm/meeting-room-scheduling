a666c08a5031dd678f68387eeec172545879a3ea
Jordan Kahtava <j.kahtava@cityssm.on.ca>
2026-02-20 09:04:39 -0500

    data returned from api needed to be destructured otherwise it was returning data.data

--- lib/server/public.ts
+++ lib/server/public.ts
@@ -1,62 +1,55 @@
 "use server";
 
 export async function fetchPublicEvents(date: string) {
-	const url = `${process.env.NEXT_PUBLIC_BASE_URL}/api/public/events?date=${date}`;
-	return privateServerFetch(url, "Failed to fetch events");
+  const url = `${process.env.NEXT_PUBLIC_BASE_URL}/api/public/events?date=${date}`;
+  return privateServerFetch(url, "Failed to fetch events");
 }
 
 export async function fetchPublicRooms() {
-	const url = `${process.env.NEXT_PUBLIC_BASE_URL}/api/public/rooms`;
-	return privateServerFetch(url, "Failed to fetch rooms");
+  const url = `${process.env.NEXT_PUBLIC_BASE_URL}/api/public/rooms`;
+  return privateServerFetch(url, "Failed to fetch rooms");
 }
 
 export async function fetchPublicConfiguration() {
-	const url = `${process.env.NEXT_PUBLIC_BASE_URL}/api/public/configuration`;
-	return privateServerFetch(url, "Failed to fetch configurations", { next: { revalidate: 1440, tags: ["public_configuration"] } });
+  const url = `${process.env.NEXT_PUBLIC_BASE_URL}/api/public/configuration`;
+  return privateServerFetch(url, "Failed to fetch configurations", {
+    next: { revalidate: 1440, tags: ["public_configuration"] },
+  });
 }
 
-// lib/server/privateFetch.ts
-export async function privateServerFetch<T>(
-	url: string,
-	contextMessage: string,
-	options: RequestInit = {},
-): Promise<{ ok: boolean; data: T | null; error: string | null }> {
-	try {
-		const res = await fetch(url, {
-			...options,
-			headers: {
-				...(options.headers || {}),
-				"x-internal-api-key": process.env.PRIVATE_INTERNAL_API_KEY!,
-			},
-			cache: "no-store",
-		});
+export async function privateServerFetch(
+  url: string,
+  contextMessage: string,
+  options: RequestInit = {},
+): Promise<{ ok: boolean; data: unknown; error: string | null }> {
+  return fetch(url, {
+    ...options,
+    headers: {
+      "Content-Type": "application/json",
+      ...options.headers,
+      "x-internal-api-key": process.env.PRIVATE_INTERNAL_API_KEY!,
+    },
+    cache: "no-store",
+  })
+    .then(async (res) => {
+      const json = await res.json().catch(() => ({}));
 
-		if (!res.ok) {
-			const errorData = await res.json().catch(() => ({}));
-			const apiMessage = errorData.message || res.statusText;
+      if (res.ok) {
+        return { ok: true, ...json, error: null };
+      }
 
-			return {
-				ok: false,
-				data: null,
-				error: `${contextMessage}: ${apiMessage}`,
-			};
-		}
-
-		const json = (await res.json()) as T;
-
-		return {
-			ok: true,
-			data: json,
-			error: null,
-		};
-	} catch (err: unknown) {
-		let message = "Unknown error";
-		if (err instanceof Error) message = err.message;
-
-		return {
-			ok: false,
-			data: null,
-			error: `${contextMessage}: ${message}`,
-		};
-	}
+      const message = json.message || res.statusText;
+      return {
+        ok: false,
+        data: null,
+        error: `${res.status} - ${contextMessage}: ${message}`,
+      };
+    })
+    .catch((err) => {
+      return {
+        ok: false,
+        data: null,
+        error: `${contextMessage}: ${err instanceof Error ? err.message : "Network Error"}`,
+      };
+    });
 }
