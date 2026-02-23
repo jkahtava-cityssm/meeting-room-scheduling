dda689c0abf1918e6c56498fde3cf8a78815fcdc
Jordan Kahtava <j.kahtava@cityssm.on.ca>
2026-02-23 14:41:49 -0500

    added a clone of the configuration settings so it can be reversed and previewed, before being submitted. updated page so it tracks changes properly.

--- app/features/system-configuration/configuration-table.tsx
+++ app/features/system-configuration/configuration-table.tsx
@@ -9,10 +9,59 @@ import { RoleComboBox } from "../roles/role-combobox";
 import { TConfigurationEntry } from "@/lib/data/configuration";
 import { Input } from "@/components/ui/input";
 import { GenericSelect } from "@/components/shared/GenericSelect";
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
 
 export function ConfigurationPage() {
-  const { data } = useConfigurationQuery();
+  const { data: serverConfiguration } = useConfigurationQuery();
+  const [workingConfiguration, setWorkingConfiguration] = useState<TConfigurationEntry[] | undefined>(undefined);
+  const [isChanged, setChanged] = useState(false);
+  //const [resourceActions, setResourceActions] = useState<ResourceActions | undefined>(undefined);
+
+  useEffect(() => {
+    if (serverConfiguration) {
+      setWorkingConfiguration(structuredClone(serverConfiguration));
+    }
+  }, [serverConfiguration]);
+
+  const handleSave = useCallback(
+    (key: string, value: string) => {
+      if (key === "visibleHoursStart") {
+        const end = workingConfiguration?.find((c) => c.key === "visibleHoursEnd")?.value;
+        if (Number(value) >= (end as number)) {
+          console.error("Start hour must be before end hour");
+          return;
+        }
+        if (Number(value) < 1) {
+          console.error("Start hour must be equal to or greater than 1");
+          return;
+        }
+      }
+
+      if (key === "visibleHoursEnd") {
+        const start = workingConfiguration?.find((c) => c.key === "visibleHoursStart")?.value;
+        if (Number(value) <= (start as number)) {
+          console.error("End hour must be after start hour");
+          return;
+        }
+        if (Number(value) > 24) {
+          console.error("End hour must be equal to or less than 24");
+          return;
+        }
+      }
+      setChanged(true);
+      setWorkingConfiguration((prev) =>
+        prev
+          ? prev.map((set) => {
+              if (set.key !== key) return set;
+
+              return { ...set, value: value } as TConfigurationEntry;
+            })
+          : prev,
+      );
+    },
+    [workingConfiguration],
+  );
 
   const isLoading = false;
 
@@ -20,26 +69,12 @@ export function ConfigurationPage() {
     return <Skeleton className="w-full h-full" />;
   }
 
-  console.log("Configuration Data:", data);
-
-  const handleSave = (key: string, newValue: string) => {
-    // Cross-field validation logic
-    if (key === "visibleHoursStart") {
-      const end = data?.find((c) => c.key === "visibleHoursEnd")?.value;
-      if (Number(newValue) >= (end as number)) {
-        return console.error("Start hour must be before end hour");
-      }
-    }
-
-    //updateMutation.mutate({ key, value: newValue });
-  };
-
   return (
     <div className="flex flex-col h-full w-full">
       <div className="flex-1 overflow-auto bg-background p-4">
         {/* The Container is the Grid */}
         <div className="grid grid-cols-[min-content_1fr] items-center gap-y-0 ">
-          {data?.map((entry) => (
+          {workingConfiguration?.map((entry) => (
             // We use React.Fragment or just flat divs because
             // the children of the grid must be the columns themselves.
             <React.Fragment key={entry.key}>
@@ -59,8 +94,39 @@ export function ConfigurationPage() {
           ))}
         </div>
       </div>
+      <footer className="flex h-14 items-center border-t bg-background px-4 shrink-0">
+        <div className="flex w-full items-center justify-end gap-2">
+          <Button
+            disabled={!isChanged}
+            variant="ghost"
+            onClick={() => {
+              setWorkingConfiguration(serverConfiguration);
+              setChanged(false);
+            }}
+          >
+            Cancel
+          </Button>
+          <Button
+            disabled={!isChanged}
+            onClick={() => {
+              //const differences = getDifferences(serverPermissions, workingPermissions);
+              //onSave(differences);
+            }}
+          >
+            Save Changes
+          </Button>
+        </div>
+      </footer>
     </div>
   );
+}
+
+function setConfigField(sets: TConfigurationEntry[], key: string, newValue: boolean): TConfigurationEntry[] {
+  return sets.map((set) => {
+    if (set.key !== key) return set;
+
+    return { ...set, value: newValue } as TConfigurationEntry;
+  });
 }
 
 const ConfigField = ({ entry, onChange }: { entry: TConfigurationEntry; onChange: (val: string) => void }) => {
@@ -104,7 +170,6 @@ const CONFIG_OVERRIDES: Record<string, React.FC<{ entry: TConfigurationEntry; on
   singleSignOnEnabled: ({ entry, onChange }) => {
     return (
       <div className="flex items-center gap-4">
-        <Switch checked={entry.value as boolean} disabled />
         <RegisterSSO isDisabled={entry.value as boolean} />
       </div>
     );
@@ -123,9 +188,7 @@ const CONFIG_OVERRIDES: Record<string, React.FC<{ entry: TConfigurationEntry; on
       ]}
       selectedValue={entry.value.toString()}
       isLoading={false}
-      onChange={function (value: string): void {
-        throw new Error("Function not implemented.");
-      }}
+      onChange={(value) => onChange(value)}
       getId={(item) => item.key.toString()}
       getLabel={(item) => item.label}
     />
@@ -136,9 +199,7 @@ const CONFIG_OVERRIDES: Record<string, React.FC<{ entry: TConfigurationEntry; on
     return (
       <RoleComboBox
         selectedRoleId={String(entry.value)}
-        onRoleChange={function (id: string, label: string): void {
-          throw new Error("Function not implemented.");
-        }}
+        onRoleChange={(id, label) => onChange(id)}
         className={""}
       ></RoleComboBox>
     );
