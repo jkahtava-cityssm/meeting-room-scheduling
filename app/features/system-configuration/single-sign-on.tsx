014e2c3ead7285a8c104fdc509eab49dde394473
Jordan Kahtava <j.kahtava@cityssm.on.ca>
2026-02-23 14:41:03 -0500

    modified the SSO button so it shows a Microsoft themed Label once activated, and added an Alert verifying the action was requested.

--- app/features/system-configuration/single-sign-on.tsx
+++ app/features/system-configuration/single-sign-on.tsx
@@ -1,15 +1,28 @@
 "use client";
 
-import { MicrosoftButton } from "@/components/ui/microsoft-signin-button";
+import { MicrosoftButton, MicrosoftLabel } from "@/components/ui/microsoft-signin-button";
 import { fetchPOST } from "@/lib/fetch";
 import { usePublicConfiguration } from "@/lib/services/public";
-import { Loader2Icon } from "lucide-react";
+import { CheckIcon, CheckSquare, Loader2Icon, X } from "lucide-react";
 import Image from "next/image";
 import { useState } from "react";
+
+import {
+  AlertDialog,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogSave,
+} from "@/components/ui/alert-dialog";
 
 export function RegisterSSO({ isDisabled }: { isDisabled?: boolean }) {
   const [disabled, setDisabled] = useState(isDisabled);
   const [pending, setPending] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
 
   async function onRegisterSSO() {
     if (pending || disabled) return;
@@ -26,18 +39,72 @@ export function RegisterSSO({ isDisabled }: { isDisabled?: boolean }) {
 
   return (
     <>
-      <MicrosoftButton onClick={() => onRegisterSSO()} disabled={disabled} aria-busy={pending}>
-        {pending && <Loader2Icon className="animate-spin" />}
-        {!pending && (
-          <Image
-            src="/images/ms-symbollockup_mssymbol_19.svg"
-            alt="An image of the crest and wreath of the city of Sault Ste. Marie"
-            width={21}
-            height={21}
-          />
-        )}
-        Register SSO
-      </MicrosoftButton>
+      {disabled ? (
+        <>
+          <MicrosoftLabel>
+            <Image
+              src="/images/ms-symbollockup_mssymbol_19.svg"
+              alt="An image of the crest and wreath of the city of Sault Ste. Marie"
+              width={21}
+              height={21}
+            />
+            Single Sign On Configured
+            <CheckIcon className="w-4 h-4 text-green-600" />
+          </MicrosoftLabel>
+        </>
+      ) : (
+        <MicrosoftButton onClick={() => setShowAlert(true)} disabled={disabled} aria-busy={pending}>
+          {pending && <Loader2Icon className="animate-spin" />}
+          {!pending && (
+            <Image
+              src="/images/ms-symbollockup_mssymbol_19.svg"
+              alt="An image of the crest and wreath of the city of Sault Ste. Marie"
+              width={21}
+              height={21}
+            />
+          )}
+          Activate Microsoft Entra SSO
+        </MicrosoftButton>
+      )}
+      <WarningDialog
+        showAlert={showAlert}
+        onContinue={() => {
+          setShowAlert(false);
+          onRegisterSSO();
+        }}
+        setShowAlert={setShowAlert}
+      ></WarningDialog>
     </>
   );
 }
+
+const WarningDialog: React.FC<{
+  showAlert: boolean;
+  onContinue: () => void;
+
+  setShowAlert: (show: boolean) => void;
+}> = ({ showAlert, onContinue, setShowAlert }) => (
+  <AlertDialog open={showAlert}>
+    <AlertDialogContent>
+      <AlertDialogHeader>
+        <AlertDialogTitle>Confirm: SSO Registration Request</AlertDialogTitle>
+        <AlertDialogDescription>
+          This action cannot be reversed from within the application, and requires manual intervention.
+        </AlertDialogDescription>
+      </AlertDialogHeader>
+
+      <AlertDialogFooter>
+        <AlertDialogAction onClick={onContinue}>Continue</AlertDialogAction>
+
+        <AlertDialogCancel onClick={() => setShowAlert(false)}>Cancel </AlertDialogCancel>
+      </AlertDialogFooter>
+    </AlertDialogContent>
+  </AlertDialog>
+);
+
+/*
+<MicrosoftLabel className="text-green-600">
+          Single Sign On Configured
+          <CheckIcon className="w-4 h-4 " />
+        </MicrosoftLabel>
+*/
--- components/ui/microsoft-signin-button.tsx
+++ components/ui/microsoft-signin-button.tsx
@@ -12,7 +12,7 @@ function MicrosoftButton({ className, ...props }: React.ComponentProps<"button">
         "not-italic font-semibold text-[15px] ",
         "h-[41px] gap-3 pl-3 pr-3",
         "bg-[#FFFFFF] text-[##5e5e5e] hover:bg-[#F9F9F9]/90",
-        "dark:bg-[#2F2F2F]  dark:text-[FFFFFF]   dark:hover:bg-[#2F2F2F]/90"
+        "dark:bg-[#2F2F2F]  dark:text-[FFFFFF]   dark:hover:bg-[#2F2F2F]/90",
       )}
       style={{ fontFamily: "Segoe UI, sans-serif" }}
       {...props}
@@ -20,4 +20,24 @@ function MicrosoftButton({ className, ...props }: React.ComponentProps<"button">
   );
 }
 
-export { MicrosoftButton };
+function MicrosoftLabel({ className, ...props }: React.ComponentProps<"span">) {
+  return (
+    <span
+      data-slot="label"
+      className={cn(
+        className,
+        "inline-flex items-center justify-center whitespace-nowrap transition-all  [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none  aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive  ",
+        "not-italic font-semibold text-[15px] ",
+        "h-[41px] gap-3",
+        "bg-background",
+        "text-[##5e5e5e] ",
+        " dark:text-[FFFFFF]  ",
+        className,
+      )}
+      style={{ fontFamily: "Segoe UI, sans-serif" }}
+      {...props}
+    />
+  );
+}
+
+export { MicrosoftButton, MicrosoftLabel };
