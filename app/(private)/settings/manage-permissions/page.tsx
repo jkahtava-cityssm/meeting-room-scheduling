2ca9bb8990c2b2daa576584f7ee4c95a46f8ae27
Jordan Kahtava <j.kahtava@cityssm.on.ca>
2026-02-24 13:47:05 -0500

    removed unnecessary div that was breaking layout

--- app/(private)/settings/manage-permissions/page.tsx
+++ app/(private)/settings/manage-permissions/page.tsx
@@ -1,10 +1,6 @@
-import { PermissionGrid } from "@/app/features/permissions/permission-grid";
+import { PermissionGrid } from "@/app/features/permissions/permission-layout";
 import * as React from "react";
 
 export default function PermissionsPage() {
-  return (
-    <div className="overflow-hidden rounded-xl border min-w-92">
-      <PermissionGrid yesLabel="YES" noLabel="NO" tooltipForDenied="Ask an admin to grant access" />
-    </div>
-  );
+  return <PermissionGrid yesLabel="YES" noLabel="NO" tooltipForDenied="Ask an admin to grant access" />;
 }
