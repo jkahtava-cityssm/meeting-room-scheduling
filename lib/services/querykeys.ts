862f5906a878bf233018dbec54c85ca0b31036af
jkahtava-cityssm <j.kahtava@cityssm.on.ca>
2026-03-06 00:15:43 -0500

    adjusted querykey to have unknown for now to stop typescript complaint

--- lib/services/querykeys.ts
+++ lib/services/querykeys.ts
@@ -2,7 +2,7 @@ export const queryKeys = {
 	configuration: {
 		all: ["configuration"] as const,
 		lists: () => [...queryKeys.configuration.all, "list"] as const,
-		list: (filters: any) => [...queryKeys.configuration.lists(), { filters }] as const,
+		list: (filters: unknown) => [...queryKeys.configuration.lists(), { filters }] as const,
 		details: () => [...queryKeys.configuration.all, "detail"] as const,
 		detail: (id: string) => [...queryKeys.configuration.details(), id] as const,
 	},
