bb8f9fa37698b506e065d9fc0335b47811a0888b
Jordan Kahtava <j.kahtava@cityssm.on.ca>
2026-03-27 13:34:55 -0400

    removed loading spinners

--- app/(private)/calendar/loading.tsx
+++ app/(private)/calendar/loading.tsx
@@ -10,14 +10,10 @@ export function CalendarLoadingPage() {
   return (
     <>
       <div className="gap-4 border-b p-4 h-22">
-        <Skeleton className="p-4 h-full flex justify-center  items-center">
-          <LoaderCircle className="animate-spin" />
-        </Skeleton>
+        <Skeleton className="p-4 h-full flex justify-center  items-center"></Skeleton>
       </div>
       <div className="p-4 h-[80vh]">
-        <Skeleton className="p-4 h-full  flex justify-center  items-center">
-          <LoaderCircle className="animate-spin" />
-        </Skeleton>
+        <Skeleton className="p-4 h-full  flex justify-center  items-center"></Skeleton>
       </div>
     </>
   );
