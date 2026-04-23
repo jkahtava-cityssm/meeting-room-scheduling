fc09464b3ff3812b2d00b81a783bf75436e56eca
Jordan Kahtava <j.kahtava@cityssm.on.ca>
2026-04-23 16:30:05 -0400

    added a table to store the system process information, its probably not necessary but i like storing it in the database instead of using a flat file.

---
+++ prisma/migrations/20260423162950_added_system_process_table/migration.sql
@@ -0,0 +1,24 @@
+-- CreateTable
+CREATE TABLE "system_process" (
+    "system_process_id" TEXT NOT NULL,
+    "key" TEXT NOT NULL,
+    "tag" TEXT NOT NULL,
+    "pid" INTEGER NOT NULL,
+    "parameter" TEXT NOT NULL,
+    "requires_restart" BOOLEAN NOT NULL DEFAULT false,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "created_by" INTEGER NOT NULL,
+    "updated_by" INTEGER NOT NULL,
+
+    CONSTRAINT "system_process_pkey" PRIMARY KEY ("system_process_id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "system_process_key_key" ON "system_process"("key");
+
+-- AddForeignKey
+ALTER TABLE "system_process" ADD CONSTRAINT "system_process_created_by_fkey" FOREIGN KEY ("created_by") REFERENCES "user"("user_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "system_process" ADD CONSTRAINT "system_process_updated_by_fkey" FOREIGN KEY ("updated_by") REFERENCES "user"("user_id") ON DELETE RESTRICT ON UPDATE CASCADE;
