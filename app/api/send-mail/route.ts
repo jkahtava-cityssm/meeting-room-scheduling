b30ead9e1d5e456284ce8d185ae3a5132e1c16b9
Jordan Kahtava <j.kahtava@cityssm.on.ca>
2026-06-01 09:00:20 -0400

    updated the send-email route to use the new functions and format

--- app/api/send-mail/route.ts
+++ app/api/send-mail/route.ts
@@ -1,8 +1,10 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { sendEmail } from '@/lib/email';
+import { sendEmail, sendEventNotificationEmail } from '@/lib/email';
 import { guardRoute } from '@/lib/api-guard';
 import { verifySecretHeader } from '@/lib/server/verifySecretHeader';
 import { UnauthorizedMessage } from '@/lib/api-helpers';
+import { getMeetingResponseEmailTemplate } from '@/lib/emails/html-templates/meeting-response';
+import { format } from 'date-fns';
 
 export async function POST(request: NextRequest) {
   /*if (!verifySecretHeader(request)) {
@@ -17,16 +19,25 @@ export async function POST(request: NextRequest) {
       if (!recipientEmail || !sessionUserEmail) {
         return NextResponse.json({ error: 'Missing emails' }, { status: 400 });
       }
+      const today = new Date().toLocaleString();
 
-      await sendEmail(recipientEmail, [sessionUserEmail], {
-        status: 'APPROVED',
-        title: 'Test Event',
-        employeeName: 'John Doe',
-        date: new Date().toLocaleString(),
-        duration: '30 Minutes',
-        notifiedNames: '',
-        room: 'Biggings Room',
-      });
+      await sendEmail(
+        recipientEmail,
+        [sessionUserEmail],
+        'Test Email',
+        getMeetingResponseEmailTemplate({
+          header: 'THIS IS THE HEADER',
+          date: format(today, 'yyyy-MM-dd hh:mm a'),
+          duration: '30 Minutes',
+          description: 'DESCRIPTION OF THE EVENT',
+          employeeName: 'John Doe',
+          notifiedNames: 'USER 1, USER 2, USER 3',
+          room: 'Biggings Room',
+          status: 'APPROVED',
+          title: 'Test Event',
+          bookingURL: '/bookings/user-view?view=day&selectedDate=' + format(today, 'yyyy-MM-dd'),
+        }),
+      );
 
       return NextResponse.json({ message: 'Email sent successfully' }, { status: 200 });
     } catch (error) {
